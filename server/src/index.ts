98500df0f84d55c6ab248cb3c10167de8892c057
unknown <dilip.sahu@targetpeak.in>
2025-04-24 16:04:02 +0530

    initial server setup

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response} from 'express';
+import "dotenv/config";
+import cors from 'cors';
+
+const app: Application = express();
+const PORT = process.env.PORT || 7000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
