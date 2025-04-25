349f991572356786261672d20f1c336fc0653e6d
unknown <dilip.sahu@targetpeak.in>
2025-04-25 16:32:46 +0530

    front install Ui.shadcn and setup

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
 
 export default function App() {
   return (
     <div>
       <h1 className='text-red-500'>I am Home page</h1>
+      <Button className='bg-red-500'>Click Me</Button>
     </div>
   )
 }
